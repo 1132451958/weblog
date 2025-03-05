39bf569b66d585ccd22ee117dbcf2b7c7595f0e1
chai <1132451958@qq.com>
2025-03-05 15:00:39 +0800

    第一次提交

---
+++ src/main/java/com/chai/weblogspringboot/WeblogSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chai.weblogspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chai/weblogspringboot/WeblogSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chai.weblogspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
